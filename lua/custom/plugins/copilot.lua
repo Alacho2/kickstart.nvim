a703fa95ca2fd087de16c1069c85deaa6fc20a56
Håvard Nordlie Mathisen <havard.nordlie.mathisen@vegvesen.no>
2026-04-15 08:41:37 +0200

    Add copilot support

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -10,7 +10,8 @@ return {
         hide_during_completion = true,
         debounce = 75,
         keymap = {
-          accept = '<M-l>',
+          accept = '<Tab>',
+          -- accept = '<M-l>',
           accept_word = false,
           accept_line = false,
           next = '<M-]>',
